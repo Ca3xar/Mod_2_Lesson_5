771d9a881be62ee3ef8a93a14b49d5b4cf021e3c
Ca3xar <Oster.corey@gmail.com>
2024-10-15 18:39:52 -0500

    Add files via upload

--- question_1_task_1&2&3.py
+++ question_1_task_1&2&3.py
@@ -1,22 +1,42 @@
-operator = input("enter an operator (* / + -): ")
-num1 = float(input("enter the first number: "))
-num2 = float(input("enter the second number: "))
-
-
-if operator == "*":
-    result = num1 * num2
-    print(round(result,  5))
-elif operator == "/":
-    try:
-        result = num1 / num2
-    except ZeroDivisionError:
-        result = 0
-    print(round(result,  5))  
-elif operator == "+":
-    result = num1 + num2
-    print(round(result,  5))
-elif operator == "-":
-    result = num1 - num2
-    print(round(result,  5))
-else:
-    print(f"{operator} is not valid")
+def multiply(x, y):
+    return x * y
+def divide(x, y):
+    if y == 0:
+        return "x" + "/" + "y"
+    return x / y
+def add(x, y):
+    return x + y
+def subtract(x, y):
+    return x - y
+def calculator():
+    print("1. multiply")
+    print("2. divide")
+    print("3. add")
+    print("4. subtract")
+    print("5. quit")
+    while True:
+        operation = input("enter operator: ")
+            
+        if operation.lower() == '5':
+                print("Farewell...")
+                break
+        if operation in ("1","2","3","4"):
+                try:
+                    x = float(input("enter the first number: "))
+                    y = float(input("enter the second number: "))
+                except ValueError:
+                    print("invalid input")
+                    continue
+                    
+                if operation == "1":
+                    print(f"{x} * {y}")
+                elif operation == "2":
+                    print(f"{x} / {y}")
+                elif operation == "3":
+                    print(f"{x} + {y}")
+                elif operation == "4":
+                    print(f"{x} - {y}")
+        else:
+            print("invalid operation")
+if __name__ == "__main__":
+    calculator()
